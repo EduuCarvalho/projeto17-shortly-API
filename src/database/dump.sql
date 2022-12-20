d37e7bc97700c146dfe3caa74858c9701fa5e91b
EduuCarvalho <educarvalho.engcivil@gmail.com>
2022-12-20 17:56:33 -0300

    feat: dump das tabelas do BD e inicio de rotas

---
+++ src/database/dump.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE shortly;
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  email TEXT NOT NULL UNIQUE,
+  password TEXT NOT NULL,
+   "createdAt" TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE sessions (
+   id SERIAL PRIMARY KEY,
+   token TEXT NOT NULL UNIQUE,
+   "userId" INTEGER NOT NULL REFERENCES users(id),
+    "createdAt" TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE urls (
+  id SERIAL PRIMARY KEY,
+  url TEXT NOT NULL,
+  "shortUrl" TEXT NOT NULL UNIQUE,
+  "visitCount" INTEGER NOT NULL DEFAULT 0,
+  "userId" INTEGER REFERENCES users(id),
+   "createdAt" TIMESTAMP DEFAULT NOW()
+);
